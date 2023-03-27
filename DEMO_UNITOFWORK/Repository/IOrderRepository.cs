7a1bca4ced483c394c31e77444ef652013af8a53
Minh Hoang <nguyenminhbrian95@gmail.com>
2023-03-27 19:09:32 +0700

    complete repository

---
+++ DEMO_UNITOFWORK/Repository/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using DEMO_UNITOFWORK.Models;
+
+namespace DEMO_UNITOFWORK.Repository
+{
+    public interface IOrderRepository
+    {
+        List<Order> GetOrders();
+        Order OrderGetOrder(int id);
+        void AddOrder(Order order);
+        void UpdateOrder(Order order);
+        void DeleteOrder(Order order);
+
+    }
+}
---
+++ DEMO_UNITOFWORK/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using DEMO_UNITOFWORK.Models;
+
+namespace DEMO_UNITOFWORK.Repository
+{
+    public interface IProductRepository
+    {
+        List<Product> GetProducts();
+        Product GetProduct(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int productId);  
+
+    }
+}
